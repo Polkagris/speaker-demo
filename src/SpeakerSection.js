e4c54019f633d5bb703b88d83876c590b81cdf98
unknown <arne.n.bjerkaas@gmail.com>
2019-11-08 18:42:52 +0100

    wip

--- src/SpeakerSection.js
+++ src/SpeakerSection.js
@@ -2,17 +2,68 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function SpeakerSection(props) {
-  //const [dataObjectState, setDataObjectState] = useState([]);
   const [speakerObjectState, setSpeakerObjectState] = useState([]);
-  /*   axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=*apikeyhere*&language=en-US&page=1`)
-  .then(res => {
-    const posts = res.data.results.map(obj => ({title: obj.title, overview: obj.overview}));
-    this.setState({ posts });
-  });
-} */
+  const [nameInputState, setNameInputState] = useState("");
 
   const uname = "appdemo";
   const pass = "en kort demo";
+
+  const addNewSpeaker = () => {
+    console.log("clicked add:", nameInputState);
+    const speakerObject = { name: { nameInputState } };
+    
+    fetch(
+      `https://planet9test.neptune-software.com:8081/api/entity/festival-speakers`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          name: "Testing Testington"
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      }
+    ).then(response => {
+      return response.json();
+    });
+/* 
+    Axios.put(
+      `https://planet9test.neptune-software.com:8081/api/entity/festival-speakers`,
+      { speakerObject },
+
+      {
+        //mode: "no-cors",
+
+        auth: {
+          username: uname,
+          password: pass
+        },
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          crossorigin: true
+        }
+      }
+    )
+      .then(res => {
+        setSpeakerObjectState(res.data);
+        console.log("AXIOS PUT CALL:", speakerObjectState);
+      })
+      .catch(console.error);
+    //console.log("AXIOS PUT CALL ERROR:", speakerObjectState);
+  }; */
+
+  const handleNameChange = e => {
+    console.log("input value:", e.target.value);
+    setNameInputState(e.target.value);
+  };
+
+  // CREATE NEW SPEAKER - PUT
+  // Input field and button
+  // On button click -> Axios put call
+
+  // OPPDATE SPEAKER - POST
+  // DELETE SPEAKER - DELETE
 
   useEffect(() => {
     Axios.get(
@@ -30,17 +81,18 @@ function SpeakerSection(props) {
       })
       .catch(console.error);
   }, []);
-
-  console.log("Data in state: ", speakerObjectState);
-
-  //let content = dataObjectState.Array.prototype.map()(speaker => speaker.id);
-
-  //console.log("content:", content);
-
+  /*     console.log("Data in state: ", speakerObjectState);
+   */
   return (
     <div className="App">
       <div>This is frontend for a change m8!</div>
       <p>{props.title}</p>
+      <label htmlFor="">
+        Name
+        <input type="text" onChange={handleNameChange} />
+        <button onClick={addNewSpeaker}>Add</button>
+      </label>
+
       <div>
         {speakerObjectState.map(it => (
           <ul>
